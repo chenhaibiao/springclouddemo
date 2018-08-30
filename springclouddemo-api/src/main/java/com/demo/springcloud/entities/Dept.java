46132144ecef7621642cc62db5aebabe9da35166
chenhaibiao <13161027080@163.com>
2018-08-30 14:22:51 +0800

    debug

--- springclouddemo-api/src/main/java/com/demo/springcloud/entities/Dept.java
+++ springclouddemo-api/src/main/java/com/demo/springcloud/entities/Dept.java
@@ -21,9 +21,6 @@ public class Dept implements Serializable {
     /**来自那个数据库，因为微服务架构可以一个服务对应一个数据库，同一个信息被存储到不同数据库*/
     private String db_source;
 
-    public Dept(){
-
-    }
     public Dept(String dname) {
         super();
         this.dname = dname;
